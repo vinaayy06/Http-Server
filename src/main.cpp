dd8c78c958962ae60de68b3d4291af58492add20
vinaayy06 <vinaysehra0606@gmail.com>
2026-07-08 04:08:16 +0530

    Respond with HTTP 200 OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,77 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
 #include <cstring>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <netdb.h>
+#include <sys/socket.h>
+
 using namespace std;
 
 int main(int argc, char **argv) {
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  std::cout << "Logs from your program will appear here!\n";
-  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-    cerr << "Failed to create server socket\n";
-    return 1;
-  }
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-    std::cerr << "setsockopt failed\n";
-    return 1;
-  }
-  
-  struct sockaddr_in server_addr;
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(4221);
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    cerr << "Failed to bind to port 4221\n";
-    return 1;
-  }
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    cerr << "listen failed\n";
-    return 1;
-  }
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  
-  cout << "Waiting for a client to connect...\n";
-  
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  cout << "Client connected\n";
-  
-  close(server_fd);
+    cout << unitbuf;
+    cerr << unitbuf;
 
-  return 0;
+    cout << "Logs from your program will appear here!" << endl;
+
+    // Create socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd < 0) {
+        cerr << "Failed to create socket" << endl;
+        return 1;
+    }
+
+    // Allow address reuse
+    int yes = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+
+    // Server address
+    sockaddr_in server_addr{};
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(4221);
+
+    // Bind socket
+    if (bind(server_fd, (sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        cerr << "Bind failed" << endl;
+        return 1;
+    }
+
+    // Start listening
+    if (listen(server_fd, 5) < 0) {
+        cerr << "Listen failed" << endl;
+        return 1;
+    }
+
+    while (true) {
+
+        sockaddr_in client_addr{};
+        socklen_t client_addr_len = sizeof(client_addr);
+
+        int client_fd = accept(
+            server_fd,
+            (sockaddr *)&client_addr,
+            &client_addr_len
+        );
+
+        if (client_fd < 0) {
+            continue;
+        }
+
+        // HTTP Response
+        string response = "HTTP/1.1 200 OK\r\n\r\n";
+
+        send(
+            client_fd,
+            response.c_str(),
+            response.size(),
+            0
+        );
+
+        close(client_fd);
+    }
+
+    close(server_fd);
+
+    return 0;
 }
