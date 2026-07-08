6bb92381c53c6ab534027486670da85765cf3da8
vinaayy06 <vinaysehra0606@gmail.com>
2026-07-08 22:51:11 +0530

    Gzip compression

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,33 @@
 #include <thread>
 #include <fstream>
 #include <sstream>
+#include <zlib.h>         
 
 using namespace std;
 string directory = "";
+string gzip_compress(const string& data) {
+    z_stream zs;
+    memset(&zs, 0, sizeof(zs));
+    deflateInit2(&zs, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 15 + 16, 8, Z_DEFAULT_STRATEGY);
+    zs.next_in = (Bytef*)data.data();
+    zs.avail_in = data.size();
 
+    string compressed = "";
+    char buffer[32768];
+
+    int result;
+
+    do {
+        zs.next_out = (Bytef*)buffer;
+        zs.avail_out = sizeof(buffer);
+        result = deflate(&zs, Z_FINISH);
+        int bytes_written = sizeof(buffer) - zs.avail_out;
+        compressed.append(buffer, bytes_written);
+
+    } while (result == Z_OK);
+    deflateEnd(&zs);
+    return compressed;
+}
 void handle_client(int client_fd) {
     char buffer[4096] = {};
     recv(client_fd, buffer, sizeof(buffer), 0);
@@ -27,7 +50,7 @@ void handle_client(int client_fd) {
     int ua_start = request.find(ua_key);
 
     if (ua_start != string::npos) {
-        ua_start += ua_key.length();       
+        ua_start += ua_key.length();
         int ua_end = request.find("\r\n", ua_start);
         user_agent = request.substr(ua_start, ua_end - ua_start);
     }
@@ -36,34 +59,27 @@ void handle_client(int client_fd) {
     int ae_start = request.find(ae_key);
 
     if (ae_start != string::npos) {
-
-        ae_start += ae_key.length();        
-        int ae_end = request.find("\r\n", ae_start); 
+        ae_start += ae_key.length();
+        int ae_end = request.find("\r\n", ae_start);
         accept_encoding = request.substr(ae_start, ae_end - ae_start);
     }
-
-    cout << "Accept-Encoding: " << accept_encoding << endl;
     bool supports_gzip = (accept_encoding.find("gzip") != string::npos);
 
-    if (supports_gzip) {
-        cout << "Client supports gzip!" << endl;
-    } else {
-        cout << "Client does NOT support gzip." << endl;
-    }
+    cout << "Supports gzip: " << (supports_gzip ? "yes" : "no") << endl;
     string body = "";
     int body_start = request.find("\r\n\r\n");
     if (body_start != string::npos) {
-        body = request.substr(body_start + 4); 
+        body = request.substr(body_start + 4);
     }
     string content_length_str = "";
     string cl_key = "Content-Length: ";
     int cl_start = request.find(cl_key);
-
     if (cl_start != string::npos) {
         cl_start += cl_key.length();
         int cl_end = request.find("\r\n", cl_start);
         content_length_str = request.substr(cl_start, cl_end - cl_start);
     }
+
     int content_length = 0;
     if (!content_length_str.empty()) {
         content_length = stoi(content_length_str);
@@ -74,20 +90,29 @@ void handle_client(int client_fd) {
     string response;
 
     if (path == "/") {
+
         response = "HTTP/1.1 200 OK\r\n\r\n";
 
     } else if (path.substr(0, 6) == "/echo/") {
         string echo_body = path.substr(6);
 
         response = "HTTP/1.1 200 OK\r\n";
-        if (supports_gzip) {
-            response += "Content-Encoding: gzip\r\n";
-        }
 
-        response += "Content-Type: text/plain\r\n";
-        response += "Content-Length: " + to_string(echo_body.length()) + "\r\n";
-        response += "\r\n";      // blank line separates headers from body
-        response += echo_body;
+        if (supports_gzip) {
+            string compressed_body = gzip_compress(echo_body);
+            response += "Content-Encoding: gzip\r\n";
+            response += "Content-Type: text/plain\r\n";
+            response += "Content-Length: " + to_string(compressed_body.length()) + "\r\n";
+
+            response += "\r\n";
+            response += compressed_body;
+
+        } else {
+            response += "Content-Type: text/plain\r\n";
+            response += "Content-Length: " + to_string(echo_body.length()) + "\r\n";
+            response += "\r\n";
+            response += echo_body;
+        }
 
     } else if (path == "/user-agent") {
         response = "HTTP/1.1 200 OK\r\n";
@@ -104,6 +129,7 @@ void handle_client(int client_fd) {
             ifstream file(filepath);
 
             if (file.is_open()) {
+
                 stringstream file_contents;
                 file_contents << file.rdbuf();
                 string file_body = file_contents.str();
@@ -116,6 +142,7 @@ void handle_client(int client_fd) {
                 response += file_body;
 
             } else {
+
                 response = "HTTP/1.1 404 Not Found\r\n\r\n";
             }
 
@@ -126,8 +153,6 @@ void handle_client(int client_fd) {
 
                 file << body;
                 file.close();
-
-
                 response = "HTTP/1.1 201 Created\r\n\r\n";
 
             } else {
@@ -139,8 +164,8 @@ void handle_client(int client_fd) {
     } else {
         response = "HTTP/1.1 404 Not Found\r\n\r\n";
     }
-
     send(client_fd, response.c_str(), response.length(), 0);
+
     close(client_fd);
 }
 
@@ -153,7 +178,6 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     int opt = 1;
@@ -170,7 +194,6 @@ int main(int argc, char* argv[]) {
     cout << "Server is running on port 4221..." << endl;
 
     while (true) {
-
         int client_fd = accept(server_fd, NULL, NULL);
         thread t(handle_client, client_fd);
         t.detach();
