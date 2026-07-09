16538484a37f92b663dc8cc5c3b67bab159f3050
vinaayy06 <vinaysehra0606@gmail.com>
2026-07-09 15:09:33 +0530

    Connection closure

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,9 @@
 #include <zlib.h>
 
 using namespace std;
+
 string directory = "";
+
 string gzip_compress(const string& data) {
     z_stream zs;
     memset(&zs, 0, sizeof(zs));
@@ -19,23 +21,20 @@ string gzip_compress(const string& data) {
     string compressed = "";
     char buffer[32768];
     int result;
-
     do {
         zs.next_out = (Bytef*)buffer;
         zs.avail_out = sizeof(buffer);
         result = deflate(&zs, Z_FINISH);
         int bytes_written = sizeof(buffer) - zs.avail_out;
         compressed.append(buffer, bytes_written);
-
     } while (result == Z_OK);
     deflateEnd(&zs);
-
     return compressed;
 }
-string handle_request(const string& request) {
+string handle_request(const string& request, bool should_close) {
+
     int first_space = request.find(' ');
     string method = request.substr(0, first_space);
-
     int second_space = request.find(' ', first_space + 1);
     string path = request.substr(first_space + 1, second_space - first_space - 1);
 
@@ -44,7 +43,6 @@ string handle_request(const string& request) {
     string user_agent = "";
     string ua_key = "User-Agent: ";
     int ua_start = request.find(ua_key);
-
     if (ua_start != string::npos) {
         ua_start += ua_key.length();
         int ua_end = request.find("\r\n", ua_start);
@@ -53,13 +51,14 @@ string handle_request(const string& request) {
     string accept_encoding = "";
     string ae_key = "Accept-Encoding: ";
     int ae_start = request.find(ae_key);
-
     if (ae_start != string::npos) {
         ae_start += ae_key.length();
         int ae_end = request.find("\r\n", ae_start);
         accept_encoding = request.substr(ae_start, ae_end - ae_start);
     }
+
     bool supports_gzip = (accept_encoding.find("gzip") != string::npos);
+
     string body = "";
     int body_start = request.find("\r\n\r\n");
     if (body_start != string::npos) {
@@ -81,47 +80,61 @@ string handle_request(const string& request) {
     if (content_length > 0 && body.length() > (size_t)content_length) {
         body = body.substr(0, content_length);
     }
+
+    auto add_connection_close = [&](string& resp) {
+        if (should_close) {
+            size_t header_end = resp.find("\r\n\r\n");
+            if (header_end != string::npos) {
+                resp.insert(header_end, "\r\nConnection: close");
+            }
+        }
+    };
     string response;
 
     if (path == "/") {
+
         response = "HTTP/1.1 200 OK\r\n\r\n";
+        add_connection_close(response);
 
     } else if (path.substr(0, 6) == "/echo/") {
+
         string echo_body = path.substr(6);
         response = "HTTP/1.1 200 OK\r\n";
 
         if (supports_gzip) {
             string compressed_body = gzip_compress(echo_body);
-
             response += "Content-Encoding: gzip\r\n";
             response += "Content-Type: text/plain\r\n";
             response += "Content-Length: " + to_string(compressed_body.length()) + "\r\n";
             response += "\r\n";
+            add_connection_close(response);
             response += compressed_body;
-
         } else {
             response += "Content-Type: text/plain\r\n";
             response += "Content-Length: " + to_string(echo_body.length()) + "\r\n";
             response += "\r\n";
+            add_connection_close(response);
             response += echo_body;
         }
 
     } else if (path == "/user-agent") {
+
         response = "HTTP/1.1 200 OK\r\n";
         response += "Content-Type: text/plain\r\n";
         response += "Content-Length: " + to_string(user_agent.length()) + "\r\n";
         response += "\r\n";
+        add_connection_close(response);
         response += user_agent;
 
     } else if (path.substr(0, 7) == "/files/") {
+
         string filename = path.substr(7);
         string filepath = directory + filename;
 
         if (method == "GET") {
+
             ifstream file(filepath);
-
             if (file.is_open()) {
-
                 stringstream file_contents;
                 file_contents << file.rdbuf();
                 string file_body = file_contents.str();
@@ -131,35 +144,38 @@ string handle_request(const string& request) {
                 response += "Content-Type: application/octet-stream\r\n";
                 response += "Content-Length: " + to_string(file_body.length()) + "\r\n";
                 response += "\r\n";
+                add_connection_close(response);
                 response += file_body;
-
             } else {
-
                 response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                add_connection_close(response);
             }
 
         } else if (method == "POST") {
+
             ofstream file(filepath);
-
             if (file.is_open()) {
-
                 file << body;
                 file.close();
                 response = "HTTP/1.1 201 Created\r\n\r\n";
-
+                add_connection_close(response);
             } else {
-
                 response = "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+                add_connection_close(response);
             }
         }
 
     } else {
+
         response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        add_connection_close(response);
     }
 
     return response;
 }
+
 void handle_client(int client_fd) {
+
     while (true) {
         char buffer[4096] = {};
         int bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
@@ -167,21 +183,24 @@ void handle_client(int client_fd) {
             cout << "Client disconnected." << endl;
             break;
         }
+
         string request = buffer;
         bool should_close = (request.find("Connection: close") != string::npos);
-        string response = handle_request(request);
+        string response = handle_request(request, should_close);
         send(client_fd, response.c_str(), response.length(), 0);
         if (should_close) {
-            cout << "Client requested connection close." << endl;
+            cout << "Closing connection as requested." << endl;
             break;
-
         }
+
         cout << "Waiting for next request on same connection..." << endl;
     }
+
     close(client_fd);
 }
 
 int main(int argc, char* argv[]) {
+
     for (int i = 1; i < argc; i++) {
         if (string(argv[i]) == "--directory") {
             if (i + 1 < argc) {
@@ -190,8 +209,8 @@ int main(int argc, char* argv[]) {
             }
         }
     }
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     int opt = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
